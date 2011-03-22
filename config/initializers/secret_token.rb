43bdd181fe1c142dc68ce9cf59bb72d72ef3e3ec
Torstein Nicolaysen <tnicolaysen@gmail.com>
2011-03-22 20:53:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BekkFotball::Application.config.secret_token = '88f1abf2a2fcb3c5b24de22f9eedc475f88519967bb7cb6d3d39df5a5da573222af847dd81a39099c4a5646f5f8780579613c727d65882981ad73583b48c8311'
